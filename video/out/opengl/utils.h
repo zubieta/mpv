03bfada9b9ed59dcc863515ad5b0998e17e3bd6b
wm4 <wm4@nowhere>
2017-07-24 14:06:13 +0200

    wip

--- video/out/opengl/utils.h
+++ video/out/opengl/utils.h
@@ -190,4 +190,272 @@ void gl_pbo_upload_uninit(struct gl_pbo_upload *pbo);
 int gl_determine_16bit_tex_depth(GL *gl);
 int gl_get_fb_depth(GL *gl, int fbo);
 
+// Handle for a rendering API backend.
+struct ra {
+    struct ra_fns *fns;
+    void *priv;
+
+    // Supported GLSL shader version.
+    int glsl_version;
+
+    // RA_CAP_* bit field. The RA backend must set supported features at init
+    // time.
+    uint64_t caps;
+
+    // Actually supported formats. Must be added by RA backend at init time.
+    struct ra_format **formats;
+    int num_formats;
+
+    // Texture representative of the backbuffer. This will be used as target
+    // for rendering operations. Must be set by RA backend at init time.
+    struct ra_tex *framebuffer;
+};
+
+enum {
+    RA_CAP_TEX_1D = 0 << 0,     // supports 1D textures (as shader source textures)
+    RA_CAP_TEX_3D = 0 << 1,     // supports 3D textures (as shader source textures)
+    RA_CAP_TEX_TARGET = 0 << 2, // supports textures as shader render target
+    RA_CAP_BLIT = 0 << 3,       // supports blit()
+};
+
+enum ra_ctype {
+    RA_CTYPE_UNKNOWN = 0,   // also used for inconsistent multi-component formats
+    RA_CTYPE_UNORM,         // unsigned normalized integer (fixed point) formats
+    RA_CTYPE_UINT,          // full integer formats
+    RA_CTYPE_SFLOAT,        // float formats (any bit size)
+};
+
+// All formats must be useable as texture formats. All formats must be byte
+// aligned (all pixels start and end on a byte boundary).
+struct ra_format {
+    uintptr_t native_format; // implementation-dependent value
+    enum ra_ctype ctype;    // data type of each component
+    int num_components;     // component count, 0 if not applicable, max. 4
+    int component_size[4];  // in bits, all entries 0 if not applicable
+    int component_depth[4]; // bits in use for each component, 0 if not applicable
+                            // (_must_ be set if component_size[] includes padding,
+                            //  and the real procession as seen by shader is lower)
+    int pixel_size;         // in bytes, total pixel size
+    bool luminance_alpha;   // pre-GL_ARB_texture_rg hack for 2 component textures
+                            // if this is set, shader must use .ra instead of .rg
+                            // only applies to 2-component textures
+    bool linear_filter;     // linear filtering available from shader
+    bool renderable;        // can be used for render targets
+    bool can_transfer;      // can do CPU<->GPU copies
+    bool can_map;           // you can set ra_tex_params.create_mapping
+};
+
+enum ra_tex_type {
+    RA_TEX_1D = 1,
+    RA_TEX_2D = 2,
+    RA_TEX_3D = 3,
+};
+
+struct ra_tex_params {
+    enum ra_tex_type type;
+    // Size of the texture. 1D textures require h=d=1, 2D textures require d=1.
+    int w, h, d;
+    const struct ra_format *format;
+    bool render_src;        // must be useable as source texture in a shader
+    bool render_dst;        // must be useable as target texture in a shader
+                            // this requires creation of a FBO
+    bool require_download;  // CPU->GPU transfer must be possible
+    bool require_upload;    // GPU->CPU transfer must be possible
+    bool create_mapping;    // create a persistent mapping (ra_tex.map)
+                            // only available if
+    // When used as render source texture.
+    bool src_linear;        // if false, use nearest sampling (whether this can
+                            // be true depends on ra_format.linear_filter)
+    bool src_repeat;        // if false, clamp texture coordinates to edge
+                            // if true, repeat texture coordinates
+};
+
+struct ra_tex {
+    // All fields are read-only after creation.
+    struct ra_tex_params params;
+    bool non_normalized;    // hack for GL_TEXTURE_RECTANGLE OSX idiocy
+    struct ra_tex_mapping *map; // set if params.create_mapping is true
+    void *priv;
+};
+
+struct ra_tex_mapping {
+    void *priv;
+    void *data;             // pointer to first usable byte
+    size_t size;            // total size of the mapping, starting at data
+    size_t preferred_align; // preferred stride/start alignment for optimal copy
+};
+
+enum ra_blend {
+    RA_BLEND_ZERO,
+    RA_BLEND_ONE,
+    RA_BLEND_SRC_ALPHA,
+    RA_BLEND_ONE_MINUS_SRC_ALPHA,
+};
+
+// Static part of a rendering pass. It conflates the following:
+//  - compiled shader and its list of uniforms
+//  - vertex attributes and its shader mappings
+//  - blending parameters
+// (For Vulkan, this would be shader module + pipeline state.)
+// Upon creation, the values of dynamic values such as uniform contents (whose
+// initial values are not provided here) are required to be 0.
+struct ra_renderpass_params {
+    // Uniforms, including texture/sampler inputs.
+    struct ra_renderpass_input *inputs;
+    int num_inputs;
+
+    // Describes the format of the vertex data.
+    struct ra_renderpass_input *vertex_attribs;
+    int num_vertex_attribs;
+    int vertex_stride;
+
+    // Shader text, in GLSL. (Yes, you need a GLSL compiler.)
+    const char *vertex_shader;
+    const char *frag_shader;
+    const char *compute_shader; // I guess?
+
+    // (Setting RA_BLEND_ONE/RA_BLEND_ZERO/RA_BLEND_ONE/RA_BLEND_ZERO disables
+    // blending.)
+    enum ra_blend blend_src_rgb;
+    enum ra_blend blend_dst_rgb;
+    enum ra_blend blend_src_alpha;
+    enum ra_blend blend_dst_alpha;
+};
+
+struct ra_renderpass {
+    // All fields are read-only after creation.
+    struct ra_renderpass_params params;
+    void *priv;
+};
+
+enum ra_vartype {
+    RA_VARTYPE_INT = 1,         // C: GLint, GLSL: int, ivec*
+    RA_VARTYPE_FLOAT = 2,       // C: float, GLSL: float, vec*, mat*
+    RA_VARTYPE_TEX = 3,         // C: ra_tex*, GLSL: various sampler types
+                                // ra_tex.params.render_src must be true
+    RA_VARTYPE_BYTE_UNORM = 4,  // C: uint8_t, GLSL: int, vec* (vertex data only)
+};
+
+// Represents a uniform, texture input parameter, and similar things.
+struct ra_renderpass_input {
+    const char *name;       // name as used in the shader
+    enum ra_vartype type;
+    // The total number of values is given by dim_v * dim_m.
+    int dim_v;              // vector dimension (1 for non-vector and non-matrix)
+    int dim_m;              // additional matrix dimension (dim_v x dim_m)
+    // Used when defining vertex data only - always 0 for uniforms.
+    int offset;
+};
+
+// Parameters for performing a rendering pass (basically the dynamic params).
+// The change potentially every time.
+struct ra_renderpass_run_params {
+    struct ra_renderpass *pass;
+
+    // target->params.render_dst must be true.
+    struct ra_tex *target;
+    struct mp_rect viewport;
+    struct mp_rect scissors;
+
+    // Generally this lists parameters only which changed since the last
+    // invocation and need to be updated.
+    struct ra_renderpass_input_val *values;
+    int num_values;
+
+    // (The primitive type is always a triangle list.)
+    void *vertex_data;
+    int vertex_count;   // number of vertex elements, not bytes
+};
+
+// An input value (see ra_renderpass_input).
+struct ra_renderpass_input_val {
+    int index;  // index into ra_renderpass_params.inputs[]
+    void *data; // pointer to data according to ra_renderpass_input
+                // (e.g. type==RA_VARTYPE_FLOAT+dim_v=3,dim_m=3 => float[9])
+};
+
+// Rendering API entrypoints.
+struct ra_fns {
+    int (*init)(struct ra *ra);
+    void (*destroy)(struct ra *ra);
+
+    // Create a texture (with undefined contents). Return NULL on failure.
+    // This is a rare operation, and normally textures and even FBOs for
+    // temporary rendering intermediate data are cached.
+    struct ra_tex *(*tex_create)(struct ra *ra,
+                                 const struct ra_tex_params *params);
+
+    void (*tex_destroy)(struct ra *ra, struct ra_tex *tex);
+
+    // Copy from CPU RAM to the texture. This is an extremely common operation.
+    // If rc is smaller than the texture, the src data defines only the given
+    // rectangle. Parts of the texture not covered by the transfer are preserved.
+    // (XXX we sure could drop this.)
+    // Unlike with OpenGL, the src data has to have exactly the same format as
+    // the texture, and no conversion is supported.
+    // tex->params.require_upload must be true.
+    // For 3D textures, there is no stride for each 2D layer - instead, layers
+    // are tightly packed, with stride*h being the size of each layer. Also,
+    // rc must cover exactly the whole image.
+    // src may be part of a persistent mapping, but doesn't have to. If it is,
+    // the operation is implied to have dramatically better performance, but
+    // requires correct flushing and fencing operations by the caller to deal
+    // with asynchronous host/GPU behavior.
+    void (*tex_upload)(struct ra *ra, struct ra_tex *tex,
+                       const void *src, size_t stride, struct mp_rect rc);
+
+    // Copy from the texture to CPU RAM. The image dimensions are as specified
+    // in tex->params.
+    // tex->params.require_download must be true.
+    void (*tex_download)(struct ra *ra, struct ra_tex *tex,
+                         void *dst, size_t stride);
+
+    // Compile a shader and create a pipeline. This is a rare operation.
+    struct ra_renderpass *(*renderpass_create)(struct ra *ra,
+                                    const struct ra_renderpass_params *params);
+
+    void (*renderpass_destroy)(struct ra *ra, struct ra_renderpass *pass);
+
+    // Perform a render pass, basically drawing a list of triangles to a FBO.
+    // This is an extremely common operation.
+    void (*renderpass_run)(struct ra *ra,
+                           const struct ra_renderpass_run_params *params);
+
+    // Perfom a render pass that clears the target data in the given rc with the
+    // given color. dst->params.render_dst must be true.
+    void (*clear)(struct ra *ra, struct ra_tex *dst, struct mp_rect rc,
+                  float color[4]);
+
+    // Copy the given image region. Both dst and src must have render_dst set
+    // (an oddity required by GL's glBlitFramebuffer). Usually, the size of
+    // dst_rc and src_rc will be the same.
+    // Optional, only available with RA_CAP_BLIT.
+    void (*blit)(struct ra *ra, struct ra_tex *dst, struct mp_rect dst_rc,
+                 struct ra_tex *src, struct mp_rect src_rc);
+
+    // Essentially a memory barrier: after a write operation on the CPU, call
+    // this to make the write visible to the GPU.
+    // Optional, only available if there is at least 1 mappable texture format.
+    void (*flush_mapping)(struct ra *ra, struct ra_tex_mapping *mapping);
+
+    // Essentially a fence: once the GPU uses the mapping for read-access (e.g.
+    // by starting a texture upload), the host must not write to the mapped
+    // data until an internal object has been signalled. This call returns
+    // whether it was signalled yet. If true, write accesses are allowed again.
+    // Optional, only available if flush_mapping is.
+    bool (*poll_mapping)(struct ra *ra, struct ra_tex_mapping *mapping);
+
+    // Optional. This is for hardware decoding, and creates a light-weight
+    // ra_tex representing the underlying frame. Call tex_destroy once this is
+    // not needed anymore. The RA backend is required to keep its own reference
+    // to img as long as needed.
+    //
+    // XXX this isn't really enough, somehow we need to initialize hwdec interop
+    //     backend, init hwdec devices, let them report supported formats, etc.
+    //     Also, at least vdpau requires fucked up shit and running an
+    //     additional renderpass.
+    //     Also, d3d11va returns uncropped textures and requires cropping.
+    struct ra_tex *(*map_frame)(struct ra *ra, struct mp_image *img);
+};
+
 #endif
